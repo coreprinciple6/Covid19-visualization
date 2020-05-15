4796172ec755238eb1f6dbd0e816cdebe6375769
Ashraf-Raneem <48803665+Ashraf-Raneem@users.noreply.github.com>
2020-05-16 03:51:29 +0600

    graphics visuals changed

--- website/map.js
+++ website/map.js
@@ -1,1 +1,21 @@
-$('.folium-map').fadeIn('slow',400)
+
+$('.pieChart').hide()
+$('.lineGraph').hide()
+
+$('.pieChartBtn').click(function(){
+    $('pieChartBtn')
+    $('.pieChart').show()
+    $('.lineGraph').hide()
+})            
+$('.lineGraphBtn').click(function(){
+    $('.lineGraph').show()
+    $('.pieChart').hide()
+})   
+
+/*Fetching the data*/
+getData()
+async function getData (){
+    const response = await fetch('../dataset/covid.csv');
+    const data = await response.text()
+    console.log()
+}
